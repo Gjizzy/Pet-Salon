02a46d2d358b7e5339bffbb2374dab8e2e2a38c3
Gjizzy <Yaquelin.Lopez@sdgku.edu>
2025-07-01 20:50:07 -0700

    Add files via upload

---
+++ Pet Salon/Pet Salon.js
@@ -0,0 +1,41 @@
+function iterateArrayWithForLoop(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+
+const pets = [
+  { name: 'Bubbles', type: 'dog', favoriteTreat: 'bacon' },
+  { name: 'Pincher', type: 'cat', favoriteTreat: 'bones' },
+  { name: 'Tizzy', type: 'dog', favoriteTreat: 'beef' }
+];
+
+iterateArrayWithForLoop(pets);
+
+
+const pets = [
+  {
+    Name: "Bubbles",
+    Age: 8,
+    Gender: "Male",
+    Service: "Walking",
+    Breed: "Bernice Mountain Dog"
+  },
+  {
+    Name: "Pincher",
+    Age: 4,
+    Gender: "Female",
+    Service: "Grooming",
+    Breed: "Persian Cat"
+  },
+  {
+    Name: "Tizzy",
+    Age: 6,
+    Gender: "Male",
+    Service: "Training",
+    Breed: "Doberman Pinscher"
+  }
+];
+
+
